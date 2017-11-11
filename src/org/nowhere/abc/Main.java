787160e524a1e38dbdf4c1baee8ead9aee95b769
sebigher <sebigher@gmail.com>
2017-11-11 07:18:46 +0200

    some history

--- src/org/nowhere/abc/Main.java
+++ src/org/nowhere/abc/Main.java
@@ -1,8 +1,25 @@
 package org.nowhere.abc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+     // jdk 1.5 - 6
+     List<String> strings = new ArrayList<String>();
+     System.out.println(strings);
+
+     //jdk 1.7 diamond operator
+     List<String> ops = new ArrayList<>();
+
+     //can you add primitive types to a list?
+     //thanks to autoboxing, you can
+     List<Integer> ints = new ArrayList<>();
+     ints.add(1);ints.add(5);
+     System.out.println(ints); //[1, 5]
+
+
     }
 }
