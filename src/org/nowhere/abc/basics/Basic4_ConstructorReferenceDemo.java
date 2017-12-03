99d5770996d91a2b251bb9a911f2341a33153ef7
Unknown <sebigher@gmail.com>
2017-12-03 20:45:00 +0200

    functionalities basics

---
+++ src/org/nowhere/abc/basics/Basic4_ConstructorReferenceDemo.java
@@ -0,0 +1,101 @@
+package org.nowhere.abc.basics;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Basic4_ConstructorReferenceDemo {
+  public static void main(String ...args){
+    //we have a list of names
+    //we want to build a list of Person objects
+    //with those names
+
+    String[] namesStrings = {"Grace Hopper", "Barbara Liskov", "Ada Lovelace",
+            "Karen Spärck Jones"};
+    //going through the streams to reach list
+    //[] ========> Stream<> ========> List<>
+    Stream<String> namesStream = Stream.of(namesStrings);
+    Stream<String> stringStream = Arrays.stream(namesStrings);
+
+    List<String> namesList = Stream.of(namesStrings).collect(Collectors.toList());
+    System.out.println(namesList);
+
+      List<String> namesList2 = namesStream.collect(Collectors.toList());
+      System.out.println(namesList2);
+
+
+      List<String> namesList3 = stringStream.collect(Collectors.toList());
+      System.out.println(namesList3);
+
+
+    // [] ============> List<>
+    List<String> stringList = Arrays.asList(namesStrings);
+    System.out.println(stringList);
+
+
+    //1.THNQ
+    List<Person> persons = stringList.stream()
+                           .map(name -> new Person(name))
+                           .collect(Collectors.toList());
+
+    System.out.println(persons);
+
+    //2.THNQ
+    //even if it has a default constructor
+    // the context dictates on what constructor to call
+    List<Person> personList = stringList.stream()
+                            .map(Person::new)
+                            .collect(Collectors.toList());
+    System.out.println(personList);
+
+
+
+
+     Person person1 = new Person("George Tush");
+     Person person2 = new Person("Donald Bump");
+
+     //THNQ create a stream from an object
+     List<Person> personList1 = Stream.of(person1)
+             .collect(Collectors.toList());
+
+     //same object
+     Person aPerson = personList1.get(0);
+     System.out.println(aPerson == person1); //same reference
+     System.out.println(aPerson.equals(person1)); //same object
+
+
+      //using copy constructor
+      //break the link
+      //unveiling the context:
+      //the context is a stream of Person
+      //Person::new invokes the constructor
+      //that takes a Person
+      List<Person> personList2 = Stream.of(person2)
+              .map(Person::new)
+              .collect(Collectors.toList());
+      Person bPerson = personList2.get(0);
+
+      System.out.println(bPerson == person2); //different reference
+      System.out.println(bPerson.equals(person2)); //different objects but equivalent
+
+
+      //varargs constructor
+      //we need o varargs constructor for Person
+
+     List<Person> personas =  namesList.stream()   //stream of strings
+      .map(name -> name.split(" "))//map to a stream of string arrays
+      .map(Person::new) // map to a stream of Person: context is different !!!
+      .collect(Collectors.toList()); //just collect
+
+      //do you want an array instead of list?
+      Person[] people  = namesList.stream()
+              .map(Person::new)
+              .toArray(Person[]::new);
+
+      for(Person p: people)
+      System.out.println(p);
+
+  }
+
+}
--- src/org/nowhere/abc/basics/Person.java
+++ src/org/nowhere/abc/basics/Person.java
@@ -1,5 +1,9 @@
 package org.nowhere.abc.basics;
 
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class Person {
     private String name;
 
@@ -18,10 +22,36 @@ public class Person {
         this.name = name;
     }
 
+    //copy constructor
+    public Person(Person person){
+        this.name = person.name;
+    }
+
+    //varargs constructor
+    public Person(String ...names){
+        this.name = Arrays.stream(names)
+                .collect(Collectors.joining(" "));
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name);
+    }
+
     @Override
     public String toString() {
         return "Person{" +
-                "name='" + name + '\'' +
+                "name:'" + name + '\'' +
                 '}';
     }
 }
